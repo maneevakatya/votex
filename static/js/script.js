8dfa1b47eef6aa25e52637c4f85834b6425294af
Maneeva Katya <maneeva_katya_@mail.ru>
2018-10-22 20:00:45 +0500

    catalogue

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,19 @@
 ﻿$(document).ready(function() {
 
+	$(".nav__link").click(function(e) {
+		  if ($('body').hasClass('index')) {
+			  e.preventDefault();
+			  var o = $(this).attr("href").substr(1),
+			  t = $(o).offset().top;
+			  $("html, body").animate({
+				  scrollTop: t - 50
+			  }, 1500),
+			  $(window).width() <= 1024 && $(".nav").slideToggle();
+		  }
+	});
+
+	$(".burger-menu").click(function() {
+	  $(".nav").slideToggle()
+  });
+
 });

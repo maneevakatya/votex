132764a27be4c6c0b13296011962da779a725fdc
Maneeva Katya <maneeva_katya_@mail.ru>
2018-10-23 20:20:48 +0500

    adaptive

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,8 @@
 ﻿$(document).ready(function() {
 
 	$(".nav__link").click(function(e) {
+		if($(window).width() <= 1023)
+		  $(this).next(".nav__list").slideToggle();
 		  if ($('body').hasClass('index')) {
 			  e.preventDefault();
 			  var o = $(this).attr("href").substr(1),
@@ -11,9 +13,42 @@
 			  $(window).width() <= 1024 && $(".nav").slideToggle();
 		  }
 	});
+    	$(".side-nav > .side-nav__list > .side-nav__item > .side-nav__link").click(function(e) {
+		$(".side-nav__item > .side-nav__link").next(".side-nav__list").slideUp();
+		$(this).next(".side-nav__list").slideDown();
+		$(".side-nav__item > .side-nav__link").parent().removeClass("side-nav__item--active");
+		$(this).parent().addClass("side-nav__item--active");
+	});
+
+	$(".side-nav__list ul .side-nav__link").click(function(e) {
+		$(".side-nav__list ul .side-nav__link").parent().removeClass("side-nav__item--active");
+		$(this).parent().addClass("side-nav__item--active");
+	});
 
 	$(".burger-menu").click(function() {
-	  $(".nav").slideToggle()
+	  $(".nav").slideToggle();
+	  $("body, html").toggleClass("ovh");
   });
+
+  $(".slide").owlCarousel({
+  nav: false,
+  dots: false,
+  items:4,
+  margin:10
+});
+
+
+
+	 $(".fabric-info__main-img").click(function() {
+				var index = parseInt($(this).children().attr("data-index"),10)-1;
+				$(".fabric-info__fancybox")[index].click();
+	 });
+
+	 $(".fabric-info__img").click(function() {
+				var origin = $(this).children().attr("data-original");
+				var index= $(this).children().attr("data-index");
+				$(".fabric-info__img-big").attr("data-index", index);
+				$(".fabric-info__img-big").attr("src", origin);
+	 });
 
 });
